cd07a7dd2772bd8e4af41b5f9de23a382e89db92
Py Zhao <zhaopy4721@outlook.com>
2019-06-25 12:19:30 +0800

    First complete get cite.

--- paper_get/get_and_download_paper.py
+++ paper_get/get_and_download_paper.py
@@ -5,8 +5,10 @@
 """
 
 import os
+import re
 import time
 import random
+import difflib
 import urllib
 import urllib.parse
 import urllib.request
@@ -21,11 +23,16 @@ from selenium.webdriver.support import expected_conditions as EC # available sin
 
 
 class PaperDownload:
-    def __init__(self, title_list):
+    def __init__(self, title_list, name, cite_p, url_p):
         self.driver = None
         self.soup = None
         self.source = None
+        self.real_title = None
 
+        self.name_en = name[0]
+        self.name_ch = name[1]
+        self.cite_p = cite_p
+        self.url_p = url_p
         self.title_gen = self.get_one_paper_title(title_list)
 
     def open_the_site(self):
@@ -43,17 +50,21 @@ class PaperDownload:
         except StopIteration:
             raise StopIteration
         else:
-            real_title = one_title.split("->")[0].strip()
-            print(real_title)
+            self.real_title = one_title.split("->")[0].strip()
+            print(self.real_title)
 
             input_element = self.driver.find_element_by_name("q")
-            input_element.send_keys(real_title)
+            input_element.clear()
+            input_element.send_keys(self.real_title)
             input_element.submit()
 
             try:
-                WebDriverWait(self.driver, 10).until(EC.title_contains(real_title))
+                WebDriverWait(self.driver, 10).until(EC.title_contains(self.real_title[:10]))
             except TimeoutException:
-                pass
+                print("Timeout when open site")
+                print(self.driver.title)
+                print(self.real_title)
+                raise RuntimeError
             else:
                 self.source = self.driver.page_source
 
@@ -61,7 +72,125 @@ class PaperDownload:
         pass
 
     def get_the_cite(self):
-        div0 = self.soup.find("div", "gs_res_ccl_mid")
+        div0 = self.soup.find("div", id="gs_res_ccl_mid")
+        div1s = div0.find_all("div", "gs_r")
+
+        cid_list = []
+        cid_data = {}
+        title = None
+        for div1 in div1s:
+            h3 = div1.find("h3", "gs_rt")
+            a = h3.find("a")
+            data_cid = div1.get("data-cid")
+            cid_list.append(data_cid)
+            data = {}
+            try:
+                title = a.get_text()
+            except AttributeError:
+                # The title does not have a attribute.
+                span = h3.find("span", id=data_cid)
+                title = span.get_text()
+            finally:
+                data.update({"title": title})
+
+            sim = difflib.SequenceMatcher(None, self.real_title, title).quick_ratio()
+            data.update({"sim": sim})
+
+            # Get the author and periodical
+            div2 = div1.find("div", "gs_a")
+            try:
+                author, periodical, *args = div2.get_text().split("-")
+            except AttributeError:
+                raise RuntimeError
+            else:
+                data.update({"author": author})
+                data.update({"periodical": periodical})
+
+            cid_data.update({data_cid: data})
+
+        print(cid_list)
+        print(cid_data)
+
+        # Sort the result with sim
+        cid_sort = sorted(cid_list, key=lambda cid: cid_data[cid]["sim"], reverse=True)
+        print(cid_sort)
+
+        # Find the right result with name
+        result_cid = None
+        for cid in cid_sort:
+            author = cid_data[cid]["author"]
+            if "," in author:
+                # English
+                print("The name is English")
+                author_list = [name.strip() for name in author.split(",")]
+                if self.name_en in author_list:
+                    result_cid = cid
+                    break
+            elif "，" in author:
+                # Chinese
+                print("The name is Chinese")
+                author_list = [name.strip() for name in author.split("，")]
+                if self.name_ch in author_list:
+                    result_cid = cid
+                    break
+            else:
+                # There is only one author
+                print("Only one name")
+                author_list = author
+                if self.name_en in author_list:
+                    result_cid = cid
+                    break
+                elif self.name_ch in author_list:
+                    result_cid = cid
+                    break
+        else:
+            print("Can't find the paper.")
+            raise RuntimeError
+
+        print(result_cid)
+
+        # div_result = self.driver.find_element("data-cid", result_cid)
+        # div_result = self.driver.find_element_by_css_selector("[data-cid=result_cid]")
+        div_rs = self.driver.find_elements_by_class_name("gs_r")
+        for div_r in div_rs:
+            check_cid = div_r.get_attribute("data-cid")
+            if check_cid == result_cid:
+                div_result = div_r
+                break
+        else:
+            print("Can't find result element")
+            raise RuntimeError
+
+        cite_click = div_result.find_element_by_class_name("gs_or_cit")
+        cite_click.click()
+
+        download_a = self.soup.find("a", id=result_cid)
+        download_url = ""
+        try:
+            download_url = download_a.get('href')
+        except TypeError:
+            print("This paper does't have url")
+            download_url = self.real_title + "  [X]"
+        finally:
+            self.write_in_file(url_path, download_url + "\n")
+
+        time.sleep(2)
+
+        self.source = self.driver.page_source
+        self.soup = BeautifulSoup(self.source, "html.parser")
+
+        div_cite0 = self.soup.find("div", id="gs_citt")
+
+        th = div_cite0.find(text=re.compile("MLA")).parent
+        print(th)
+        td = th.next_sibling
+        cite = td.div.get_text()
+        print(cite)
+        self.write_in_file(self.cite_p, cite + "\n")
+
+        # Click cancel for next search
+        cancel = self.driver.find_element_by_id("gs_cit-x")
+        cancel.click()
 
     def start_download(self):
         while True:
@@ -82,6 +211,14 @@ class PaperDownload:
                 self.download_paper()
 
     @classmethod
+    def write_in_file(cls, path, value):
+        try:
+            with open(path, "a+") as f:
+                f.write(value)
+        except IOError:
+            print("Write failed.")
+
+    @classmethod
     def get_one_paper_title(cls, path):
         with open(path, "r") as f:
             while True:
@@ -92,13 +229,20 @@ class PaperDownload:
 
     @classmethod
     def wait_random_time(cls):
-        time.sleep(random.uniform(1, 4))
+        time.sleep(random.uniform(0.5, 2.5))
 
 
 if __name__ == "__main__":
-
+    name_en = "S Wang"
+    name_ch = "王爽"
     tile_list = "data/test_list.txt"
-    down = PaperDownload(tile_list)
+
+    cite_path = "data/result/cite.txt"
+    url_path = "data/result/url.txt"
+
+    
+
+    down = PaperDownload(tile_list, [name_en, name_ch], cite_path, url_path)
 
     down.open_the_site()
     down.start_download()
--- paper_get/test/html_test.py
+++ paper_get/test/html_test.py
@@ -4,39 +4,103 @@
     Test google html.
 """
 
+import re
 import difflib
 from bs4 import BeautifulSoup
 
-with open("../data/test.html", "r") as f:
+with open("../data/test4.html", "r") as f:
     html = f.read()
 
 # print(html)
 
-source = "复杂分布数据的二阶段聚类算法"
+source = "基于双边滤波的极化SAR相干斑抑制"
+name_en = "S Wang"
+name_ch = "王爽"
 
 soup = BeautifulSoup(html, "html.parser")
 
 div0 = soup.find("div", id="gs_res_ccl_mid")
 
-# print(div0)
-
 div1s = div0.find_all("div", "gs_r")
 
-titles = []
-sims = []
+cid_list = []
+cid_data = {}
+title = None
 for div1 in div1s:
     h3 = div1.find("h3", "gs_rt")
     a = h3.find("a")
-    title = a.get_text()
-    titles.append(title)
+    data_cid = div1.get("data-cid")
+    cid_list.append(data_cid)
+    data = {}
+    try:
+        title = a.get_text()
+    except AttributeError:
+        # The title does not have a attribute.
+
+        span = h3.find("span", id=data_cid)
+        title = span.get_text()
+    finally:
+        data.update({"title": title})
 
     sim = difflib.SequenceMatcher(None, source, title).quick_ratio()
-    sims.append(sim)
+    data.update({"sim": sim})
 
-max_sim = max(sims)
+    # Get the author and periodical
+    div2 = div1.find("div", "gs_a")
+    try:
+        author, periodical, *args = div2.get_text().split("-")
+    except AttributeError:
+        raise RuntimeError
+    else:
+        data.update({"author": author})
+        data.update({"periodical": periodical})
 
-print(sims)
-print(max_sim)
+    cid_data.update({data_cid: data})
 
-index = sims.index(max_sim)
-print(titles[index])
+print(cid_list)
+print(cid_data)
+
+# Sort the result with sim
+cid_sort = sorted(cid_list, key=lambda cid: cid_data[cid]["sim"], reverse=True)
+print(cid_sort)
+
+# Find the right result with name
+result_cid = None
+for cid in cid_sort:
+    author = cid_data[cid]["author"]
+    if "," in author:
+        # English
+        print("The name is English")
+        author_list = [name.strip() for name in author.split(",")]
+        if name_en in author_list:
+            result_cid = cid
+            break
+    elif "，" in author:
+        # Chinese
+        print("The name is Chinese")
+        author_list = [name.strip() for name in author.split("，")]
+        if name_ch in author_list:
+            result_cid = cid
+            break
+    else:
+        # There is only one author
+        print("Only one name")
+        author_list = author
+        if name_en in author_list:
+            result_cid = cid
+            break
+        elif name_ch in author_list:
+            result_cid = cid
+            break
+else:
+    print("Can't find the paper.")
+    raise RuntimeError
+
+print(result_cid)
+
+div_cite0 = soup.find("div", id="gs_citt")
+
+th = div_cite0.find(text=re.compile("MLA")).parent
+print(th)
+td = th.next_sibling
+print(td.div.get_text())
